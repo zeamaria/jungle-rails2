f8f089fd9cbf5315ce52d69239824cff3c1ef5d2
zeamaria <lingardzea@gmail.com>
2021-06-02 21:01:58 -0700

    Add Feature: User Authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625062916) do
+ActiveRecord::Schema.define(version: 20210603035406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -53,6 +61,7 @@ ActiveRecord::Schema.define(version: 20160625062916) do
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
   end
+
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 

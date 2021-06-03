bec28d97df62e9312da0925d83e840907ca6391e
zeamaria <lingardzea@gmail.com>
2021-06-02 21:13:52 -0700

    Add RSpec setup for Jungle

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210603035406) do
+ActiveRecord::Schema.define(version: 20210603041021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -62,8 +54,20 @@ ActiveRecord::Schema.define(version: 20210603035406) do
     t.integer  "category_id"
   end
 
-
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "widgets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
